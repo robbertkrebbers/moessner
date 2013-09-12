1ed79158fbd1ac93279d1824b83a2a46c6900ad7
Robbert Krebbers <mail@robbertkrebbers.nl>
2013-09-12 15:08:45 +0200

    Update copyright notices.

--- prelude.v
+++ prelude.v
@@ -1,4 +1,4 @@
-(* Copyright (c) 2012-2013, Louis Parlant and Robbert Krebbers. *)
+(* Copyright (c) 2013, Louis Parlant and Robbert Krebbers. *)
 (* This file is distributed under the terms of the BSD license. *)
 Require Export Utf8.
 Require Export Arith Omega Setoid Morphisms List NPeano.
